efcf284646b66c823fe66f0f56423b7d162947b2
muhammadzidane632 <muhammad.zidane41183@smk.belajar.id>
2026-05-07 10:15:30 +0700

    Setup tailwind css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,9 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
+        
+    <h1 class="text-5-xl font-bold  text-center text-white"> I'm Muhammad Zidane Testing Tailwind Is work And Ready To use</h1>
+    <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
                     @auth
