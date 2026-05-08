afbed411b819501a664eb0bed400ca0ff03aed9b
muhammadzidane632 <muhammad.zidane41183@smk.belajar.id>
2026-05-08 08:06:34 +0700

    Adjust : increase partner logo size and adjust margin in mobile

--- resources/views/components/partners.blade.php
+++ resources/views/components/partners.blade.php
@@ -13,10 +13,10 @@
     ];
 @endphp
 
-<section class="bg-primary-1 pt-4 pb-ev-7" id="partners">
+<section class="bg-primary-1 pt-4 pb-16 md:pb-28" id="partners">
     <div class="container max-w-ev-container mx-auto px-4 md:px-6">
         <div class="mt-ev-4 pt-ev-4 pb-2 border-t border-gray-100">
-            <h3 class="text-center font-bold text-gray-800 text-sm sm:text-base md:text-lg mb-8">
+            <h3 class="text-center font-bold text-gray-800 text-sm sm:text-base md:text-lg mb-12 md:mb-16">
                 250+ lembaga dan instansi telah menggunakan layanan kami
             </h3>
             
@@ -26,7 +26,7 @@
                 <div class="flex items-center min-w-max animate-[ev-marquee_30s_linear_infinite] pr-12">
                     @foreach($partners as $partner)
                         <div class="flex justify-center items-center mx-4 md:mx-6 shrink-0">
-                            <img src="{{ asset('assets/images/sections/partners/' . $partner) }}" alt="Partner" class="h-12 md:h-16 w-auto object-contain">
+                            <img src="{{ asset('assets/images/sections/partners/' . $partner) }}" alt="Partner" class="h-16 md:h-[72px] w-auto object-contain">
                         </div>
                     @endforeach
                 </div>
@@ -35,7 +35,7 @@
                 <div class="flex items-center min-w-max animate-[ev-marquee_30s_linear_infinite] pr-12" aria-hidden="true">
                     @foreach($partners as $partner)
                         <div class="flex justify-center items-center mx-4 md:mx-6 shrink-0">
-                            <img src="{{ asset('assets/images/sections/partners/' . $partner) }}" alt="Partner" class="h-12 md:h-16 w-auto object-contain">
+                            <img src="{{ asset('assets/images/sections/partners/' . $partner) }}" alt="Partner" class="h-16 md:h-[72px] w-auto object-contain">
                         </div>
                     @endforeach
                 </div>
